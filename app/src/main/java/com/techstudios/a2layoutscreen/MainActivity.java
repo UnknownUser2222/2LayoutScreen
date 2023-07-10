eb3ec8f70deb060967490d134d0e053746dd6924
Muhammad Ayan <userunamed981@gmail.com>
2023-07-10 12:28:22 +0600

    Initial commit

---
+++ app/src/main/java/com/techstudios/a2layoutscreen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.techstudios.a2layoutscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello there!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/techstudios/a2layoutscreen/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.techstudios.a2layoutscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+
+    }
+}
